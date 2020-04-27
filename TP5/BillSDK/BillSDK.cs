113a1e03091784a8c87ca180a8e3a00cae7644b4
Ambre <ambre_donnadieu@live.fr>
2020-04-28 00:52:55 +0200

    SDKs finis

--- TP5/BillSDK/BillSDK.cs
+++ TP5/BillSDK/BillSDK.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace BillSDK
+public class Bill
 {
-    public class Class1
+    public User user { get; set; }
+
+    public List<BillLine> AllBillLines { get; set; }
+
+    public double TotalHT;
+    public double TotalTTC;
+
+    public Bill CreateBill(User user2, List<ItemLine> lines)
     {
+        Bill b = new Bill();
+        b.user = user2;
+        foreach(ItemLine l in lines)
+        {
+            BillLine bl = new BillLine();
+            bl.itemLine = l;
+            bl.soustotal = l.quantite_item * l.item.prix_item;
+            b.AllBillLines.Add(bl);
+        }
+        foreach(BillLine bl in b.AllBillLines)
+        {
+            b.TotalHT += bl.soustotal;
+        }
+        b.TotalTTC = b.TotalHT * 1.15; //on prend 15% de taxes ce qui correspond aux taxes au Québec
+        return b;
+    }
+
+    public class BillLine
+    {
+        public ItemLine itemLine { get; set; }
+        public double soustotal {get;set;}
     }
 }
