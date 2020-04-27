956c3d5853af7cc79d363665c3d4914c36b8ebc7
Ambre <ambre_donnadieu@live.fr>
2020-04-28 00:16:45 +0200

    first commit + sdk

---
+++ TP5/StockSDK/StockSDK.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Item
+{
+    public string nom_item { get; set; }
+
+    public double prix_item { get; set; }
+
+    public Item(string nom, double prix)
+    {
+        nom_item = nom;
+        prix_item = prix;
+    }
+
+}
+
+public class ItemLine
+{
+    public Item item { get; set; }
+    public int quantite_item { get; set; }
+
+    public ItemLine()
+    {
+        item = null;
+        quantite_item=0;
+    }
+    public ItemLine(Item i, int q)
+    {
+        item = i;
+        quantite_item = q;
+    }
+}
+
+public class StockManager
+{
+    List<ItemLine> AllLines;
+    public ItemLine ReserveItem(int quantity, string name)
+    {
+        ItemLine i = new ItemLine();
+        foreach(ItemLine line in AllLines)
+        {
+            if(line.item.nom_item == name)
+            {
+                line.quantite_item--;
+                i = line;
+            }
+        }
+        return i;
+    }
+
+    public void ReleaseItem(ItemLine line)
+    {
+        foreach(ItemLine itemline in AllLines)
+        {
+            if(itemline == line)
+            {
+                itemline.quantite_item++; //annule la reservation de l'item
+            }
+        }
+    }
+}
+
+
+
---
+++ TP5/UserSDK/UserSDK.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class User
+{
+    public string nom {get;set;}
+    public string prenom {get;set;}
+    public string email {get;set;}
+    public string username {get;set;}
+
+    public User()
+    {
+        nom = "nom";
+        prenom="prenom";
+        email= prenom+"."+nom+"@email.com";
+        username="username";
+    }
+    /*public static User GetUser(string pseudo)
+    {
+        User u = new User();
+        if(this.username ==pseudo){
+            u.nom = User.nom;
+        }
+        return ;
+    }*/
+}
